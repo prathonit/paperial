2b528ec0b0f3c6da5e8511609550893763d4a7e9
= <=>
2021-02-18 20:52:31 +0530

    setup eslint and prettier

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.status(200).json({id: 2019});
+router.get('/', (req, res, next) => {
+	res.status(200).json({ id: 2019 });
 });
 
 module.exports = router;
